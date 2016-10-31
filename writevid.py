f9be277f5dc1bde31172ff0d5852cb88a8d179ab
squeakus <jonathanbyrn@gmail.com>
2016-10-31 17:21:09 +0000

    now can output video

---
+++ writevid.py
@@ -0,0 +1,30 @@
+import numpy as np
+import cv2
+
+cap = cv2.VideoCapture(0)
+
+# Define the codec and create VideoWriter object
+fourcc = cv2.VideoWriter_fourcc(*'DIVX')
+
+#the spec is for my webcam, the IP camera is 1080x1920
+out = cv2.VideoWriter('output.avi',fourcc, 20.0, (848,480))
+
+while(cap.isOpened()):
+    ret, frame = cap.read()
+
+    if ret==True:
+        frame = cv2.flip(frame,0)
+
+        # write the flipped frame
+        out.write(frame)
+
+        cv2.imshow('frame',frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+    else:
+        break
+
+# Release everything if job is finished
+cap.release()
+out.release()
+cv2.destroyAllWindows()
