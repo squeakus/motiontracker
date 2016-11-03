761d21b05a2774012113c41d1d7cfd20a7352b9c
squeakus <jonathanbyrn@gmail.com>
2016-11-03 19:07:32 +0000

    futureproofing code

--- motiontrackaverage.py
+++ motiontrackaverage.py
@@ -1,3 +1,4 @@
+from __future__ import print_function
 import numpy as np
 import cv2
 import sys
@@ -33,7 +34,7 @@ def main(passwd):
 
 
         if avg is None:
-    		print "[INFO] starting background model..."
+    		print("[INFO] starting background model...")
     		avg = gray.copy().astype("float")
     		continue
 
@@ -43,14 +44,11 @@ def main(passwd):
 
         # dilate the thresholded image to fill in holes, then find contours
         # on thresholded image
-        thresh = cv2.dilate(thresh, None, iterations=2)
+        thresh = cv2.dilate(thresh, None, iterations=5)
         (_, cnts, _) = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,
                                         cv2.CHAIN_APPROX_SIMPLE)
 
         # loop over the contours
-        if len(cnts) > 0:
-            out.write(frame)
-
         for c in cnts:
             if cv2.contourArea(c) < min_area:
                 continue
@@ -58,7 +56,7 @@ def main(passwd):
             # and update the text
             (x, y, w, h) = cv2.boundingRect(c)
             cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
-            text = "Recording"
+            text = ""
 
         # draw the text and timestamp on the frame
         cv2.putText(frame, text, (10, 20),
@@ -68,8 +66,12 @@ def main(passwd):
 
         # show the frame and record if the user presses a key
         cv2.imshow("Security Feed", frame)
-        cv2.imshow("Thresh", thresh)
-        cv2.imshow("Frame Delta", frameDelta)
+        #cv2.imshow("Thresh", thresh)
+        #cv2.imshow("Frame Delta", frameDelta)
+
+        if len(cnts) > 0:
+            out.write(frame)
+
         key = cv2.waitKey(1) & 0xFF
 
         # if the `q` key is pressed, break from the lop
@@ -85,6 +87,63 @@ def main(passwd):
     camera.release()
     cv2.destroyAllWindows()
     out.release()
+
+# Malisiewicz et al.
+def non_max_suppression_fast(boxes, overlapThresh):
+	# if there are no boxes, return an empty list
+	if len(boxes) == 0:
+		return []
+
+	# if the bounding boxes integers, convert them to floats --
+	# this is important since we'll be doing a bunch of divisions
+	if boxes.dtype.kind == "i":
+		boxes = boxes.astype("float")
+
+	# initialize the list of picked indexes
+	pick = []
+
+	# grab the coordinates of the bounding boxes
+	x1 = boxes[:,0]
+	y1 = boxes[:,1]
+	x2 = boxes[:,2]
+	y2 = boxes[:,3]
+
+	# compute the area of the bounding boxes and sort the bounding
+	# boxes by the bottom-right y-coordinate of the bounding box
+	area = (x2 - x1 + 1) * (y2 - y1 + 1)
+	idxs = np.argsort(y2)
+
+	# keep looping while some indexes still remain in the indexes
+	# list
+	while len(idxs) > 0:
+		# grab the last index in the indexes list and add the
+		# index value to the list of picked indexes
+		last = len(idxs) - 1
+		i = idxs[last]
+		pick.append(i)
+
+		# find the largest (x, y) coordinates for the start of
+		# the bounding box and the smallest (x, y) coordinates
+		# for the end of the bounding box
+		xx1 = np.maximum(x1[i], x1[idxs[:last]])
+		yy1 = np.maximum(y1[i], y1[idxs[:last]])
+		xx2 = np.minimum(x2[i], x2[idxs[:last]])
+		yy2 = np.minimum(y2[i], y2[idxs[:last]])
+
+		# compute the width and height of the bounding box
+		w = np.maximum(0, xx2 - xx1 + 1)
+		h = np.maximum(0, yy2 - yy1 + 1)
+
+		# compute the ratio of overlap
+		overlap = (w * h) / area[idxs[:last]]
+
+		# delete all indexes from the index list that have
+		idxs = np.delete(idxs, np.concatenate(([last],
+			np.where(overlap > overlapThresh)[0])))
+
+	# return only the bounding boxes that were picked using the
+	# integer data type
+	return boxes[pick].astype("int")
 
 if __name__ == '__main__':
     if len(sys.argv) < 2:
