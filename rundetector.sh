b50fcdcdefbe0c165eb119c17f35dff20e38d475
squeakus <jonathanbyrn@gmail.com>
2016-11-24 00:14:59 +0000

    new generic flag

---
+++ rundetector.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+if [ $# -lt 1 ];
+then
+    echo "please specify detector: reundetector.sh <sift|surf|orb|brisk|akaze>"
+    exit 0
+fi
+
+source /home/pi/.virtualenvs/cv/bin/activate
+echo  "runnning with $1 detector"
+python /home/pi/motiontracker/rundetector.py -p 1 -d $1
+
